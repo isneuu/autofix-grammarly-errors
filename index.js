dcee79dd86a3da280c55d0d4c5ffee423c8e2fa8
isneuu <83936898+isneuu@users.noreply.github.com>
2022-02-01 21:22:45 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
+function refreshData() {
+    x = 3; // 3 Seconds
 
-function refreshData()
-{
-    x = 3;  // 3 Seconds
-    
     try {
- 
-if (document.querySelector('.cards-replacements_labels-itemRemove') !== null) {
-var newclass = document.querySelector('.cards-replacements_labels-itemRemove');
-newclass.click();
+
+        if (document.querySelector('.cards-replacements_labels-itemRemove') !== null) {
+            var newclass = document.querySelector('.cards-replacements_labels-itemRemove');
+            newclass.click();
+        } else {
+            var newclass = document.getElementsByClassName("cards-replacements_labels-itemInsert");
+            newclass = newclass[0];
+            newclass.click();
+        }
+    } catch (error) {
+        if (document.getElementsByClassName("wrapper_fipkee2") !== null) {
+            var cli = document.getElementsByClassName("wrapper_fipkee2");
+            var vli = cli[0];
+            vli.click();
+        }
+    }
+    setTimeout(refreshData, x * 1000);
 }
-else {
-var newclass = document.getElementsByClassName("cards-replacements_labels-itemInsert");
-newclass = newclass[0];
-newclass.click();
-}
-} catch (error) {
-if ( document.getElementsByClassName("wrapper_fipkee2") !== null) {
-var cli = document.getElementsByClassName("wrapper_fipkee2");
-var vli = cli[0];
-vli.click();
-}
-}
-    setTimeout(refreshData, x*1000);
-}
- 
-refreshData(); 
- 
+
+refreshData();
